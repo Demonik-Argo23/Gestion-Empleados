bae4ffe6983c07bfc0640f6bedaa09da9ef1fbf5
Demonik-Argo23 <cerdivaca9@gmail.com>
2025-05-24 21:05:44 -0600

    fix: ensure static files are served from the Panel directory

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static(path.join(__dirname, 'Panel')));
+
 app.get('/', (req, res) => {
     res.redirect('/login.html');
 });
